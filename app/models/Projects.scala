867e4b3c683963518a8deb2afdebfcea59741aca
Radzisław Galler <rgaller@gazeta.pl>
2013-08-17 00:06:44 +0200

    Using ReactiveMong instead of salat.

--- app/models/Projects.scala
+++ app/models/Projects.scala
@@ -1,12 +1,12 @@
 package models
 
-import com.mongodb.casbah.Imports._
-import org.scala_tools.time.Imports._
 import play.api.Play.current
-import com.novus.salat._
-import se.radley.plugin.salat._
-import com.novus.salat.dao.{ SalatDAO, ModelCompanion }
+import org.joda.time._, format._
 
+import reactivemongo.bson._
+import play.modules.reactivemongo._, ReactiveMongoPlugin._
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.api.indexes._
 /**
  * Helper for pagination.
  */
@@ -15,38 +15,38 @@ case class Page[A](items: Seq[A], page: Int, offset: Long, total: Long) {
   lazy val next = Option(page + 1).filter(_ => (offset + items.size) < total)
 }
 
-@Salat
 trait TimeStamps {
-  val created: DateTime = DateTime.now
-  var modified: DateTime = DateTime.now
 }
 
 // ------------------------------ Project ------------------------------
 case class Project(
-  @Key("_id") id: ObjectId = new ObjectId,
+  id: Option[BSONObjectID],
   name: String,
-  description: String
-) extends TimeStamps {
+  description: String,
+  created: Option[DateTime],
+  modified: Option[DateTime]
+)
 
-  def addRequirement(r: Requirement) {
-    val req = r.copy(projectId = Option(id))
-    Requirement.insert(r)
+object Project  {
+  implicit object ProjectReader extends BSONDocumentReader[Project] {
+    def read(doc: BSONDocument): Project =
+      Project(
+        doc.getAs[BSONObjectID] ("_id"         ),
+        doc.getAs[String]   ("name"        ).get,
+        doc.getAs[String]   ("description" ).get,
+        doc.getAs[BSONDateTime] ("created"     ).map  (dt => new DateTime (dt.value ) ),
+        doc.getAs[BSONDateTime] ("modified"    ).map (dt => new DateTime (dt.value  ) )
+      )
   }
-}
 
-object Project extends ModelCompanion[Project, ObjectId] {
-  val collection = getCollection("projects")
-  val dao = new SalatDAO[Project, ObjectId](collection = collection){
-
-    collection.ensureIndex(MongoDBObject("name" -> 1), "name", unique = true)
-
-    val requirements = new ChildCollection[Requirement, ObjectId](
-      collection = getCollection("requirements"), parentIdField = "projectId"){}
-    val useCases     = new ChildCollection[UseCase, ObjectId](
-      collection = getCollection("use_cases"), parentIdField = "projectId"){}
-    val glossary     = new ChildCollection[GlossaryEntry, ObjectId](
-      collection = getCollection("glossary"), parentIdField = "projectId"){}
+  implicit object ProjectWriter extends BSONDocumentWriter[Project] {
+    def write(proj: Project): BSONDocument =
+      BSONDocument(
+        "_id"         -> proj.id.getOrElse(BSONObjectID.generate),
+        "name"        -> proj.name,
+        "description" -> proj.description,
+        "created"     -> proj.created.map(d => BSONDateTime(d.getMillis)),
+        "modified"    -> proj.modified.map(d => BSONDateTime(d.getMillis))
+      )
   }
-  def findByName( name: String) =  findOne(MongoDBObject("name" -> name))
-
 }
--- project/Build.scala
+++ project/Build.scala
@@ -9,39 +9,44 @@ import org.ensime.sbt.util.SExp._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "muse"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "muse"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
+  val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
+  val sonatypeSnapshots = "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  val appDependencies = Seq(
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.10-SNAPSHOT"
+  )
 
-    val appDependencies = Seq(
-      "se.radley" %% "play-plugins-salat" % "1.2"
-    )
-
-    val main = play.Project(appName, appVersion, appDependencies,
-                           settings = Defaults.defaultSettings ++ scalaxbSettings
-                         ).settings(
-      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "style.less"),
-      // scalaxb settings
-      sourceGenerators in Compile <+= scalaxb in Compile,
-      packageName in scalaxb in Compile := "models",
-      protocolPackageName in scalaxb in Compile := Some("XuseProtocol"),
-      // salat settings
-      routesImport += "se.radley.plugin.salat.Binders._",
-      templatesImport += "org.bson.types.ObjectId",
-      // ensime settings
-      ensimeConfig := sexp(
-        key(":only-include-in-index"), sexp(
-          "controllers\\..*",
-          "models\\..*",
-          "views\\..*",
-          "play\\..*"
-        ),
-        key(":source-roots"), sexp(
-          "/home/rajish/proj/muse/muse.play/app",
-          "/home/rajish/proj/muse/muse.play/test",
-          "/home/rajish/bin/play2/framework/src"
-        )
+  val main = play.Project(appName, appVersion, appDependencies,
+    settings = Defaults.defaultSettings ++ scalaxbSettings
+  ).settings(
+    resolvers ++= Seq(sonatypeSnapshots),
+    lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "css" ** "style.less"),
+    // salat settings
+    // routesImport += "se.radley.plugin.salat.Binders._",
+    // templatesImport += "org.bson.types.ObjectId",
+    // ensime settings
+    ensimeConfig := sexp(
+      key(":only-include-in-index"), sexp(
+        "controllers\\..*",
+        "models\\..*",
+        "views\\..*",
+        "play\\..*"
+      ),
+      key(":source-roots"), sexp(
+        "/home/rajish/proj/muse/muse.play/app",
+        "/home/rajish/proj/muse/muse.play/test",
+        "/home/rajish/bin/play2/framework/src"
       )
     )
+  )
+
+  // val generate = TaskKey[Unit]("genxml", "Generates data models from XML.")
+  // val generateTask = generate := {
+  //   // scalaxb settings
+  //   sourceGenerators in Compile <+= scalaxb in Compile,
+  //   packageName in scalaxb in Compile := "models",
+  //   protocolPackageName in scalaxb in Compile := Some("XuseProtocol"),
+  // }
 }
