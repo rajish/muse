971d9b863735e13940a4cb38d618f7a5da855a39
Radzisław Galler <rgaller@gazeta.pl>
2013-08-17 01:16:12 +0200

    Looks like macros work if not using non-BSON types.

--- app/models/Projects.scala
+++ app/models/Projects.scala
@@ -23,30 +23,32 @@ case class Project(
   id: Option[BSONObjectID],
   name: String,
   description: String,
-  created: Option[DateTime],
-  modified: Option[DateTime]
+  created: Option[BSONDateTime],
+  modified: Option[BSONDateTime]
 )
 
 object Project  {
-  implicit object ProjectReader extends BSONDocumentReader[Project] {
-    def read(doc: BSONDocument): Project =
-      Project(
-        doc.getAs[BSONObjectID] ("_id"         ),
-        doc.getAs[String]   ("name"        ).get,
-        doc.getAs[String]   ("description" ).get,
-        doc.getAs[BSONDateTime] ("created"     ).map  (dt => new DateTime (dt.value ) ),
-        doc.getAs[BSONDateTime] ("modified"    ).map (dt => new DateTime (dt.value  ) )
-      )
-  }
+  implicit val projectHandler = Macros.handler[Project]
 
-  implicit object ProjectWriter extends BSONDocumentWriter[Project] {
-    def write(proj: Project): BSONDocument =
-      BSONDocument(
-        "_id"         -> proj.id.getOrElse(BSONObjectID.generate),
-        "name"        -> proj.name,
-        "description" -> proj.description,
-        "created"     -> proj.created.map(d => BSONDateTime(d.getMillis)),
-        "modified"    -> proj.modified.map(d => BSONDateTime(d.getMillis))
-      )
-  }
+  // implicit object ProjectReader extends BSONDocumentReader[Project] {
+  //   def read(doc: BSONDocument): Project =
+  //     Project(
+  //       doc.getAs[BSONObjectID] ("_id"         ),
+  //       doc.getAs[String]   ("name"        ).get,
+  //       doc.getAs[String]   ("description" ).get,
+  //       doc.getAs[BSONDateTime] ("created"     ).map  (dt => new DateTime (dt.value ) ),
+  //       doc.getAs[BSONDateTime] ("modified"    ).map (dt => new DateTime (dt.value  ) )
+  //     )
+  // }
+
+  // implicit object ProjectWriter extends BSONDocumentWriter[Project] {
+  //   def write(proj: Project): BSONDocument =
+  //     BSONDocument(
+  //       "_id"         -> proj.id.getOrElse(BSONObjectID.generate),
+  //       "name"        -> proj.name,
+  //       "description" -> proj.description,
+  //       "created"     -> proj.created.map(d => BSONDateTime(d.getMillis)),
+  //       "modified"    -> proj.modified.map(d => BSONDateTime(d.getMillis))
+  //     )
+  // }
 }
