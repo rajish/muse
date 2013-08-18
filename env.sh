5889a493fd0efe6c28c6432743da4df12a9626a5
Radzisław Galler <rgaller@gazeta.pl>
2013-08-18 02:26:45 +0200

    Fast compilation environment.

--- env.sh
+++ env.sh
@@ -1,1 +1,11 @@
 export PATH=~/bin/play2:$PATH
+
+rmdir target > /dev/null 2>&1
+mkdir target > /dev/null 2>&1
+
+# sudo mkfs -q /dev/ram1 65536
+# sudo mount /dev/ram1 target
+findmnt target
+if [  $? ]; then
+    sudo mount -t tmpfs -o size=1g none target
+fi
